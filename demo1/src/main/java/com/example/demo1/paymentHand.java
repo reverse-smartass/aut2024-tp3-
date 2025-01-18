cb47cbd0ea08b7f5d015d3e123e73578ca8911ab
Duc Minh Nguyen <107412957+reverse-smartass@users.noreply.github.com>
2025-01-18 17:09:45 -0500

    Add files via upload

---
+++ demo1/src/main/java/com/example/demo1/paymentHand.java
@@ -0,0 +1,12 @@
+package com.example.demo1;
+
+public class paymentHand extends paymentType{
+    public paymentHand(String text) {
+        super(text);
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+}
